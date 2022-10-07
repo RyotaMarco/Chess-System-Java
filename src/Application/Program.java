bf276d194a81694c48f993efaee1d79ca77dd7ee
Marcos Eduardo <marcos@nouva.se>
2022-10-07 10:40:54 -0300

    First class: Position

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,7 +1,10 @@
 package Application;
 
+import Boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.print("Hello World!!");
+        Position pos = new Position(3,5);
+        System.out.print(pos);
     }
 }
