7d483490c9daa8a41e70705697d24cdfbd37999d
Marcos Eduardo <marcos@nouva.se>
2022-10-07 15:22:00 -0300

    Placing pieces on the board

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package Chess;
 
 import Boardgame.Board;
+import Boardgame.Position;
+import Chess.ChessPieces.King;
+import Chess.ChessPieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +11,7 @@ public class ChessMatch {
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -18,5 +22,9 @@ public class ChessMatch {
         }
         return mat;
     }
-
+    private void initialSetup(){
+        board.placePiece(new Rook(board,Color.WHITE), new Position(2,1));
+        board.placePiece(new King(board,Color.BLACK),new Position(0,4));
+        board.placePiece(new King(board,Color.WHITE),new Position(7,4));
+    }
 }
