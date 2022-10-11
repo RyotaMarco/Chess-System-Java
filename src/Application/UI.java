d3370f378a5a29956ed37f2f61a8b7e81cdc5d32
Marcos Eduardo <marcos@nouva.se>
2022-10-11 10:50:07 -0300

    Check logic

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -67,6 +67,9 @@ public class UI {
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+        if (chessMatch.getCheck()){
+            System.out.println("CHECK!");
+        }
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
@@ -82,7 +85,7 @@ public class UI {
 
     private static void printPiece(ChessPiece piece, boolean background){
         if (background){
-            System.out.print(ANSI_BLUE_BACKGROUND);
+            System.out.print(ANSI_RED_BACKGROUND);
         }
         if (piece == null) {
             System.out.print("-");
